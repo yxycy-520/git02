c8102aad26a86516b57f22190f3b47c80b3f1da0
yxy <3026646453@qq.com>
2022-12-11 01:16:36 +0800

    分支操作

--- src/main/java/com/it/test/test.java
+++ src/main/java/com/it/test/test.java
@@ -4,5 +4,8 @@ public class test {
 
     public static void main(String[] args) {
         System.out.println("sssss");
+        System.out.println("11111");
+        System.out.println("122");
+        System.out.println("分支操作");
     }
 }
